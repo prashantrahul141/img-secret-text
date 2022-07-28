9ee134b0705154f3e76daeffa75d3fc24b695ecd
Prashant Rahul <prashantrahul141@protonmail.com>
2022-07-29 00:17:36 +0530

    added convert_png(), max_gap(), incode_header(), half finished incode(), base for get_secret_value()

--- incoder.py
+++ incoder.py
@@ -1,20 +1,18 @@
 # class for incoding text in image
 from PIL import Image
-from math import floor
+from math import ceil, floor
 
 class Incoder:
-    def __init__(self) -> None:
-        self.img = ""
-        self.text = ""
-        self.gaps = ""
-
-
-    def set_img_text_gaps(self, img:str, text:str, gaps:int) -> None:
-        '''sets img text and gaps given by user'''
+    def __init__(self, img:str, text:str, gaps:int) -> None:
         self.img = img
-        self.IMG = Image.open(self.img)
         self.text = text
         self.gaps = gaps
+        self.original_img = Image.open(self.img)
+        self.convert_png()
+    
+    def convert_png(self):
+        self.original_img.save("output.png")
+        self.IMG = Image.open("output.png")
 
 
     def check_length(self):
@@ -26,15 +24,39 @@ class Incoder:
 
     def max_gap(self) -> bool:
         '''returns the max gap which is possible '''
-
-        _total_length = self.IMG.size[0] * self.IMG.size[1]
-        return floor((_total_length - 1)/len(self.text))
+        _total_length = self.IMG.size[0] * self.IMG.sizimage.pnge[1]
+        _max_gap = _total_length - 1/ceil(len(self.text)/3)
+        return floor(_max_gap)
     
 
-    def incode_header(self) -> bool:
-        pass
-
+    def incode_header(self) -> None:
+        self.IMG.putpixel((0, 0), (0, len(self.text), self.gaps))
+        self.IMG.save("output.png")
+        
     def incode(self) -> bool:
+        self.incode_header()
+        
+        counter = 0
+        index_ = 0
+        text_index = 0
+        list_of_pixel_values = [(self.gaps*i + 2) for i in range(ceil(len(self.text)/3))[1:]]
+        list_of_pixel_values = [2] + list_of_pixel_values
+
+        print(list_of_pixel_values)
+        for x in range(self.IMG.size[1]):
+            for y in range(self.IMG.size[1]):
+
+                counter += 1
+                if x*y == list_of_pixel_values[index_]:
+                    index_ += 1
+
+                    # incoding text at x, y
+                    # incode
+
+                    if index_ >= len(list_of_pixel_values):
+                        return True
+
+
+    def get_secret_value(self, _index) -> tuple:
+        '''will return the secret numeric values of charatcers from the text'''
         pass
-
-
