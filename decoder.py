0796ec805014dcd87f4cf4b63621c4602faea0ee
Kinjal Kavish Boro <85004952+KeviTheDuck@users.noreply.github.com>
2022-07-29 12:55:41 +0530

    added check_header(), check_png()

--- decoder.py
+++ decoder.py
@@ -1,5 +1,25 @@
 # class for decoding text from image
+from PIL import Image
 
 class Decoder:
-    pass
+    def __init__(self, img:str):
+        self.img = img
+        self.original_img = Image.open(self.img)
+        
+
+    def check_png(self) -> bool:
+        if self.img.endswith('.png'):
+            return True
+        else:
+            return False
+
+    def check_header(self) -> bool:
+        ''' check if the first pixel has 0 in its R value '''
+        rgb_values = list(self.original_img.getdata())
+        if rgb_values[0][0] == 0:
+            return True
+        else:
+            return False
+
+
 
