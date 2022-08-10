3583c32f91d678a60b97159a40fdc5156ad7f880
Prashant <prashantrahul141@protonmail.com>
2022-08-10 11:30:59 +0530

    ACTUALLY ACTUALLY WORKING PROPERLY

--- incoder.py
+++ incoder.py
@@ -1,5 +1,4 @@
 # class for incoding text in image
-from multiprocessing.pool import IMapIterator
 from PIL import Image
 from math import ceil, floor
 
@@ -10,8 +9,11 @@ class Incoder:
         self.secret_list = secret_list
         self.convert_img()
         self.gaps = self.max_gap() if gaps == 0 else gaps
+        self.image_array_data = self.IMG.getdata()
+        print(self.gaps)
     
     def convert_img(self):
+        '''converts jpg img into png to avoid jpg compression'''
         _original_img = Image.open(self.img_name)
         _original_img.save("output.png")
         self.IMG = Image.open("output.png")
@@ -26,37 +28,43 @@ class Incoder:
     def max_gap(self) -> bool:
         '''returns the max gap which is possible '''
         _total_length = self.IMG.size[0] * self.IMG.size[1]
-        _max_gap = (_total_length - 1)/ceil(len(self.text))
+        _max_gap = _total_length/len(self.text)
         return floor(_max_gap)
     
 
     def incode_header(self) -> None:
         self.IMG.putpixel((0, 0), (0, len(self.text), self.gaps))
         self.IMG.save("output.png")
-        
+
     def incode(self) -> bool:
+        '''changes pixel r value according to secret text'''
         self.incode_header()
-        
-        counter = 0
+
+        list_of_pixel_indexes = []
+        _counter_for_text_length = 0
+
+        for i in range(1, len(self.image_array_data), self.gaps):
+            list_of_pixel_indexes.append(i)
+            if _counter_for_text_length >= len(self.text)-1:
+                break;
+            _counter_for_text_length += 1
+
+        _counter = 0
         _index = 0
 
-        list_of_pixel_values = [(self.gaps*i + 2) for i in range(ceil(len(self.text)))[1:]]
-        list_of_pixel_values = [2] + list_of_pixel_values
+        for y in range(self.IMG.size[1]+1):
+            for x in range(self.IMG.size[0]+1):
 
-        for x in range(self.IMG.size[1]):
-            for y in range(self.IMG.size[1]):
-
-                if counter == list_of_pixel_values[_index]:
-                    # incoding text at x, y
-                    self.IMG.putpixel((x, y), self.get_secret_value(_index, counter))
+                if _counter == list_of_pixel_indexes[_index]:
+                    self.IMG.putpixel((x, y), self.get_secret_value(_index, _counter))
                     self.IMG.save("output.png")
-                    
+
                     _index += 1
-                    if _index >= len(list_of_pixel_values):
+                    if _index >= len(list_of_pixel_indexes):  
                         return True
 
-                counter += 1
-        
+                _counter += 1
+    
 
     def get_secret_value(self, _index, _counter) -> tuple:
         '''will return the secret numeric values of charatcers from the text'''
@@ -64,20 +72,8 @@ class Incoder:
         _text = self.text[_index]
         if _text in self.secret_list:
             _r = self.secret_list.index(_text)
-            _data = list(self.IMG.getdata())[_counter]
-            _g, _b = _data[1], _data[2]
+            _data = list(self.image_array_data)[_counter]
+            _g, _b = _data[1], _data[2];
             return (_r, _g, _b, 255)
-
+           
         raise ValueError(f"character '{_text}' not in secret list.")
-        
-
-
-import json
-with open("secret_text_list.json", 'r') as f:
-    data = json.load(f)["list"]
-
-incoder = Incoder("test.png", "hellow", data)
-incoder.incode()
-for i in list(Image.open("output.png").getdata()):
-    print(i)
-
